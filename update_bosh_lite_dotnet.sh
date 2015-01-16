27f9c419da7cf7486be8720505d2db4fa20f38fa
Ubuntu <ubuntu@ip-10-60-0-29.us-west-2.compute.internal>
2015-01-16 22:15:49 +0000

    Saving current state, allows everthing up to setting up elastic load balancer

--- update_bosh_lite_dotnet.sh
+++ update_bosh_lite_dotnet.sh
@@ -1,28 +1,6 @@
-# Generate a deployment stub with the BOSH director UUID
-mkdir -p ~/deployments/bosh-lite
-cd ~/workspace/diego-release
-./scripts/print-director-stub > ~/deployments/bosh-lite/director.yml
-
-# Generate and target cf-release manifest:
-cd ~/workspace/cf-release
-./generate_deployment_manifest warden \
-    ~/deployments/bosh-lite/director.yml \
-    ~/workspace/diego-release/templates/enable_diego_in_cc.yml > \
-    ~/deployments/bosh-lite/cf.yml
-bosh deployment ~/deployments/bosh-lite/cf.yml
-
-# Do the BOSH dance:
-cd ~/workspace/cf-release
-bosh create release --force
-bosh -n upload release
-bosh -n deploy
-
 # Generate and target diego's manifest:
-cd ~/workspace/diego-release
-./scripts/generate-deployment-manifest bosh-lite ../cf-release \
-    ~/deployments/bosh-lite/director.yml > \
-    ~/deployments/bosh-lite/diego.yml
-bosh deployment ~/deployments/bosh-lite/diego.yml
+
+~/workspace/cf-release/generate_deployment_manifest aws /home/ubuntu/workspace/deployments/microbosh/director.yml ~/workspace/diego-release/templates/enable_diego_in_cc.yml /home/ubuntu/workspace/deployments/microbosh/terraform_settings.yml  ~/workspace/deployments/microbosh/director.yml > ~/workspace/deployments/microbosh/cf.yml
 
 # Dance some more:
 bosh create release --force
